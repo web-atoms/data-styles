5a6618622b6a9cfb7d21394ea19f8f625d30529e
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-04-18 13:20:44 +0530

    Added vertical flex

---
+++ src/styles/data-vertical-flex.ts
@@ -0,0 +1,35 @@
+import styled from "../styled";
+import vars from "../vars";
+
+styled.add({
+    "data-layout": {
+
+        "vertical-flex": styled.css `
+            display: flex;
+            flex-direction: column;
+            align-items: stretch;
+            justify-content: flex-start;
+            gap: ${vars.spacing};
+        ` ,
+
+        "vertical-flex-center-items": styled.css `
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            gap: ${vars.spacing};
+        ` ,
+
+        "vertical-flex-stretch-items": styled.css `
+            display: flex;
+            flex-direction: column;
+            align-items: stretch;
+            justify-items: stretch;
+            justify-content: flex-start;
+            padding: ${vars.spacingMedium};
+            gap: ${vars.spacing};
+        `.child("*", styled.css `
+            width: 100%;
+        `)
+    }
+})
