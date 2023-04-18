d86a6ed215acb70905c3192a0bfb0f2787498345
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-04-18 11:10:24 +0530

    Added center all

---
+++ src/styles/data-loader.ts
@@ -0,0 +1,21 @@
+import styled from "../styled";
+
+styled.add({
+    "data-layout": {
+        "center-all": styled.css `
+            width: 100%;
+            height: 100%;
+            flex: 1 1 100%;
+            align-self: stretch;
+            justify-self: stretch;
+            display: grid;
+            grid-template-columns: 1fr;
+            grid-template-rows: 1fr;
+        `.child("*", styled.css `
+            grid-row: 1;
+            grid-column: 1;
+            align-self: center;
+            justify-self: center;
+        `)
+    }
+})
