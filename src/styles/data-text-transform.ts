3e34863eabc6b4ec5903514dc0da9dd39b51b2e8
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-04-20 10:34:45 +0530

    Added transform

---
+++ src/styles/data-text-transform.ts
@@ -0,0 +1,8 @@
+import styled, { createSet } from "../styled";
+
+styled.add({
+    "data-text-transform": createSet("text-transform", [
+        "none", "capitalize", "uppercase", "lowercase", "full-width", "full-size-kana",
+        "inherit", "initial", "revert", "revert-layer", "unset"
+    ])
+});
