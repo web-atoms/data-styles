06e1ce97b80d2300ea92921fc108be1ca9db9034
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-04-24 16:39:24 +0530

    Added none in decoration

--- src/styles/data-text-decoration.ts
+++ src/styles/data-text-decoration.ts
@@ -1,12 +1,7 @@
-import styled from "../styled";
+import styled, { createSet } from "../styled";
 
 styled.add({
     "data-text-decoration": {
-        "*": styled.css `text-decoration: attr(data-text-decoration) ;`,
-        "underline": styled.css `text-decoration: underline ;`,
-        "overline": styled.css `text-decoration: overline ;`,
-        "strike-through": styled.css `text-decoration: line-through ;`,
-        "line-through": styled.css `text-decoration: line-through ;`,
-        "wavy": styled.css `text-decoration: wavy ;`,
+        ... createSet("text-decoration", ["underline", "overline", "strike-through", "line-through", "wavy", "none"])
     }
 });
