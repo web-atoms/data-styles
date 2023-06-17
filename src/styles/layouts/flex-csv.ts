ba978f8d1197a6932c598fca5e33496baf3bc353
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-06-17 16:49:11 +0530

    Added flex csv

--- src/styles/layouts/flex-csv.ts
+++ src/styles/layouts/flex-csv.ts
@@ -1,14 +1,20 @@
+
 import styled from "../../styled";
 import vars from "../../vars";
 
 styled.add({
     "data-layout": {
         
-        "flex-wrap": styled.css `
+        "flex-csv": styled.css `
             display: flex;
             gap: ${vars.spacing};
             align-items: center;
             flex-wrap: wrap;
+            & > span, & > div, & > [data-append=comma] {
+                &:not(:empty):not(:last-child)::after {
+                    content: ","
+                }
+            }
         `,
     }
 }, "low")
