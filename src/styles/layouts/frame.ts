01fcc490d27b30942d0c65cb1de7534f993bd58d
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-06-21 20:06:26 +0530

    Spacing increased

--- src/styles/layouts/frame.ts
+++ src/styles/layouts/frame.ts
@@ -6,15 +6,15 @@ styled.add({
         "frame": styled.css `
             border: solid 1px;
             border-color: ${vars.borderColor};
-            padding: ${vars.spacing};
-            border-radius: ${vars.spacing};
+            padding: ${vars.spacingLarge};
+            border-radius: ${vars.spacingLarge};
             box-shadow: 0 0 10px 1px rgb(0 0 0 / 25%);
         `,
         "frame-vertical-flex": styled.css `
             border: solid 1px;
             border-color: ${vars.borderColor};
-            padding: ${vars.spacing};
-            border-radius: ${vars.spacing};
+            padding: ${vars.spacingLarge};
+            border-radius: ${vars.spacingLarge};
             box-shadow: 0 0 10px 1px rgb(0 0 0 / 25%);
             display: flex;
             flex-direction: column;
