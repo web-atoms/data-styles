817e78bc9aa5021bbe434bb1b463f669285328a4
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-06-23 11:30:02 +0530

    Added max width

---
+++ src/styles/size/data-max-width.ts
@@ -0,0 +1,8 @@
+import styled from "../../styled";
+
+styled.add({
+    "data-max-width": {
+        "600px": styled.css `max-width: 600px`,
+        "900px": styled.css `max-width: 900px`,
+    }
+});
