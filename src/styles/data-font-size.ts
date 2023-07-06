4e8c26c5c72abc7b40a03ed20e9a2e17fa4276a4
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-07-06 12:43:06 +0530

    Added more styles

--- src/styles/data-font-size.ts
+++ src/styles/data-font-size.ts
@@ -4,8 +4,14 @@ import vars from "../vars";
 styled.add({
     "data-font-size": {
         "*": styled.css `font-size: attr(data-font-size) ;`,
-        "small": styled.css `font-size: ${vars.fontSizeSmall} ;`,
+        "small": styled.css `font-size: small;`,
+        "smaller": styled.css `font-size: smaller;`,
         "medium": styled.css `font-size: medium ;`,
         "large": styled.css `font-size: large ;`,
+        "larger": styled.css `font-size: larger ;`,
+        "x-small": styled.css `font-size: x-small;`,
+        "xx-small": styled.css `font-size: xx-small;`,
+        "x-large": styled.css `font-size: x-large;`,
+        "xx-large": styled.css `font-size: xx-large;`,
     }
 }, "high");
