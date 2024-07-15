0f08c17f2282583164f4217b7d9a54f97cadbf33
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-07-15 10:32:16 +0530

    css fix

--- src/footer.ts
+++ src/footer.ts
@@ -2,7 +2,7 @@ import styled from "./styled";
 
 export const footer = styled.css `
 
-    &[data-popup-window] &[data-layout=command-row] {
+    & [data-popup-window] > [data-window-content] > [data-layout=command-row] {
         margin: -7px;
         margin-top: 5px;
         border-radius: 0;
